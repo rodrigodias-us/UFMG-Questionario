60c8499971bd86a28feb31b7d1a2d63fc98ec684
Rodrigo Gomes Dias <rodrigo.dias@planb.com.br>
2014-05-03 12:42:57 -0300

    Cancela o envio do formulario através do botao IR

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -8,13 +8,24 @@ $(function() {
         
         $("#result-pre-form").text(dig1+dig2+dig3+dig4+dig5);
     });
+    
+    $(".marcador_linha").change(function() {
+       if($(this).is(':checked')) {
+           $(this).parents("tr").addClass("linha-marcada");
+       } else {
+           $(this).parents("tr").removeClass("linha-marcada");
+       }
+           
+    });
 	
+	// Confirmação quando Vai fechar o App
 	document.addEventListener("backbutton", function(){
 		if(confirm("Tem certeza que deseja fechar o programa?")){
 			navigator.app.exitApp();
 		}
 	},false);
 	
+	// Colori o select quando existe valor
 	$('select').on('change', function(e){
 		if($(this).val())
 		{
@@ -25,4 +36,5 @@ $(function() {
 			$(this).css('color', 'initial');
 		}
 	})
+	;
 });

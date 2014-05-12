83e6e872d9f0a42f8fff827e8071c13e7b1e0254
Rodrigo Gomes Dias <rodrigo.dias@planb.com.br>
2014-05-12 16:32:25 -0300

    Ajuste do botão exportar

--- www/js/app.js
+++ www/js/app.js
@@ -133,8 +133,8 @@ angular.module('project', [])
 				$scope.dataExport.push(JSON.parse(results.rows.item(i).data));
 			}
 			var csv = $scope.convertToCSV($scope.dataExport);
-			window.open("data:text/csv;charset=utf-8," + escape(csv));
-			// $scope.saveFile('export.csv', csv);
+			// window.open("data:text/csv;charset=utf-8," + escape(csv));
+			$scope.saveFile('export.csv', csv);
 			alert("Arquivo Exportado");
 		}
 		db.transaction(queryDB, error);
